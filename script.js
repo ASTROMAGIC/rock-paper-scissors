3a6612960a20479f051fa551b351ee820972d225
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-03-05 18:51:02 -0800

    fleshed out html and css
    wrote basic html structure and did some css writing. i need to go back and see why changing the choices from an id to a class selector made the size of the buttons actually grow

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-console.log("Hello World!")
-
-function game() {
-    function getComputerChoice() {
-        const words = ["rock", "paper", "scissor"];
-        const n = Math.floor(Math.random() * words.length);
-        
-        return words[n];
-
-    }
-    function playerRound (player Selection, computer) {
-        
-        let player = playerSelection.toLowerCase();
-
-        if (player === computer) {
-            return "Draw Match";
-        }   else if (player === 'rock') && computer === 'paper') {
-            computerScore += 1; 
-            return "You Lose ... 'Paper beats Rock'";
-        } 
 
 
-    }
-}
+
+
+// what are the components I need for this project? how do i reverse engineer what I've learned to show I can make this from scratch? 
