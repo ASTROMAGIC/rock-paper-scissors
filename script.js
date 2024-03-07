8a488fa42beff0a4f04cafd2b72c1c73e300ac1a
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-03-06 19:54:24 -0800

    added score keeping, added color
    game is now finished, will go over code during the next few days to see how i can remake it using documentation and stackoverflow only

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0; 
 let computerScore = 0;
 
@@ -13,7 +15,7 @@ let computerScore = 0;
 function playGame (playerChoice){
     
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    let result = "";
+    let result = " ";
 
     if(playerChoice === computerChoice){
         result = "IT'S A TIE!";
@@ -21,19 +23,42 @@ function playGame (playerChoice){
     else{
         switch(playerChoice){
             case "rock":
-                (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
                 break; 
             case "paper":
-                (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
                 break; 
             case "scissors": 
-                (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE!";
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
         }
     }
 
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = result;
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.remove("redText");
+            resultDisplay.classList.remove("blackText");
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.remove("greenText");
+            resultDisplay.classList.remove("blackText");
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break; 
+        case "IT'S A TIE!":
+            resultDisplay.classList.remove("redText");
+            resultDisplay.classList.remove("greenText");
+            resultDisplay.classList.add("blackText");   
+            break; 
+    }
 
     return result; 
 }
