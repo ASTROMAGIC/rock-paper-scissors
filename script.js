ea4b5c425608aec5917a93241013114f77d46d2e
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-06-19 09:44:38 -0700

    updated game ui and win condition function
    slight ui update in addition to the game resetting once a certain score has been reached. game state switches too abruptly before resetting. will need to have a winner clearly declared before the game is reset.

--- script.js
+++ script.js
@@ -60,7 +60,28 @@ function playGame (playerChoice){
             break; 
     }
 
+    if (playerScore === 5) {
+        resultDisplay.textContent = "PLAYER WINS!";
+        resetGame();
+    } else if (computerScore === 5) {
+        resultDisplay.textContent = "COMPUTER WINS!";
+        resetGame();
+    }
+
     return result;
 }
+
+// there are multiple ways to restart this game. I can either write a new function that integrates with the playGame function I have already wrote, or integrate ways of resetting the game into my switch cases. Writing the function will require less code written and is far far cleaner, so I will go that route. 
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    resultDisplay.textContent = "New Game!";
+    resultDisplay.classList.remove("redText", "greenText", "blackText");
+}
+
+// there is a slight issue with this method. It is clear who the winner is, the switching to the new game is abrupt and does not visibly declare the winner before before switching to the new game. So let's see how we can solve this. 
 
 
